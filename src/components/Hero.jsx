aff49416a721b0585d4dda19c9b2efe295840d49
MAC <glorybrain01@gmail.com>
2026-03-07 01:23:25 +0100

    implement the first part of hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,86 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import React from "react";
+
+const Hero = () => {
+  useGSAP(() => {
+    const heroSlit = new SplitText(".title", { type: "chars, words" });
+    const paragraphSlit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSlit.chars.forEach((char) => char.classList.add("text-gradient"));
+
+    gsap.from(heroSlit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+    });
+
+    gsap.from(paragraphSlit.lines, {
+      opacity: 0,
+      duration: 1.8,
+      delay: 1,
+      yPercent: 100,
+      ease: "expo.out",
+      stagger: 0.06,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          scrub: true,
+          end: "bottom top",
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+  }, []);
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">Mojito</h1>
+
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p className="text-[.96rem]">Cool. Crisp. Classic</p>
+              <p className="subtitle lg:text-start">
+                Sip the Spirit <br />
+                of Summer
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* <div className="video absolute inset-0">
+        <video muted playsInline preload="auto" src="/videos/output.mp4" />
+      </div> */}
+    </>
+  );
+};
+
+export default Hero;
